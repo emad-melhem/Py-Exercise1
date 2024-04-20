a3d40a70e2321336601794d75afce23d402d7d9e
Imad Mohamad <yehya.mh@live.com>
2024-04-20 12:37:07 +0200

    Add Exercise 4

---
+++ Exercise 4.py
@@ -0,0 +1,11 @@
+x = input("Input a numer between 100-999 :")
+try:
+    val1 = int(x)
+    if val1 <100 or val1 > 999:
+        raise IndexError("Number is out of range!")
+    y = x[::-1]
+    print(f"The reverse is {y}.")
+except ValueError:
+    print("That's not a Number!")
+except IndexError as indexerror:
+    print(indexerror.args)
